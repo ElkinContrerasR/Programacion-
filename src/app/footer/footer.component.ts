16ee0dd40f25980a96aa591cf487f0beac46121b
ElkinContrerasR <escontreras-2023a@corhuila.edu.co>
2024-09-19 07:32:46 -0500

    Add files via upload

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  
+autor: any ={nombres: 'Elkin', apellidos: 'Contreras Rojas'}
+autor1: any ={nombres: 'Alejandro', apellidos: 'Ortiz Vargas'}
+autor3: any ={curso: 'Programación 3', grupo: 'Grupo 9-corhuila'}
+
+}
